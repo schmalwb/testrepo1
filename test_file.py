06ad00bd85265aee699301039d8cfb72ea9faadf
schmalwb <87079305+schmalwb@users.noreply.github.com>
2022-10-27 14:23:27 +0200

    2nd ext

--- test_file.py
+++ test_file.py
@@ -6,3 +6,12 @@ Created on Thu Oct 27 14:06:20 2022
 """
 
 Lalilu
+
+import os
+os.chdir('C:\\Users\\DICE\\sciebo\\research\\DEAL_econ2\\orig2') #change directory as needed
+from datetime import datetime
+import pandas as pd
+from pybliometrics.scopus import ScopusSearch
+
+from pybliometrics.scopus.utils import config
+print(config['Authentication']['APIKey'])  # Show keys
